bb3f80b13cc391f07dcda2de29173634eda27b3a
David Liebemann <David.Liebemann@gmail.com>
2015-04-02 01:24:28 +0200

    update. not sure what

--- JavaProjects/zevolution/src/de/zevolution/Game.java
+++ JavaProjects/zevolution/src/de/zevolution/Game.java
@@ -26,107 +26,115 @@ import de.zevolution.physics.systems.UpdatePositionSystem;
  */
 public class Game implements ApplicationListener {
 
-    private PooledEngine engine;
-    
-    //Systems
-    private InputKeyboardSystem inputKeyboardSystem;
-    
-    // Zu Testzwecken
-    private OrthographicCamera ortho;
-    private Box2DDebugRenderer debugRenderer;
+	private PooledEngine engine;
 
-    @Override
-    public void create() {
-        // creating the Ashley engine
-        engine = new PooledEngine();
-        EntityCreator.engine = engine;
-        
-        // initialise Box2D
-        Box2D.init();
+	// Systems
+	private InputKeyboardSystem inputKeyboardSystem;
 
-        // create and add systems
-        addSystems();
-        //create and add input processors
-        setInputProcessors();
+	// Zu Testzwecken
+	private OrthographicCamera ortho;
+	private Box2DDebugRenderer debugRenderer;
 
-        //zu testzwecken
-        ortho = new OrthographicCamera();
-        ortho.setToOrtho(true, Gdx.graphics.getWidth() / EntityCreator.physicsSystem.getScale(), Gdx.graphics.getHeight()/ EntityCreator.physicsSystem.getScale());
-        debugRenderer = new Box2DDebugRenderer();
-        
-        MenuEntityCreator.createMainMenu();
-    }
+	@Override
+	public void create() {
+		// creating the Ashley engine
+		engine = new PooledEngine();
+		EntityCreator.engine = engine;
 
-    private void addSystems() {
-        
-    	// InputKeyboardSystem
-        inputKeyboardSystem = new InputKeyboardSystem(GameConstants.INPUT_PRIORITY);
-        engine.addSystem(inputKeyboardSystem);
-        
-        // add PhysicSystem
-        PhysicsSystem physicsSystem = new PhysicsSystem(
-                GameConstants.BOX2D_VELOCITY_ITERATIONS,
-                GameConstants.BOX2D_POSITIONS_ITERATIONS,
-                GameConstants.BOX2D_SCALE,
-                GameConstants.PHYSICS_PRIORITY);
+		// initialise Box2D
+		Box2D.init();
 
-        engine.addSystem(physicsSystem);
-        physicsSystem.setGravity(new Vector2(0,0)); // erstmal keine gravity, brauchen wir in unserem Spiel nicht
-        EntityCreator.physicsSystem = physicsSystem; 
-        
-        // add MovementSystem
-        engine.addSystem(new MovementSystem(GameConstants.PHYSICS_PRIORITY+1));
-        
-        //add UpdatePositionSystem
-        engine.addSystem(new UpdatePositionSystem(GameConstants.PHYSICS_PRIORITY + 2));   
-    }
-    
-    /**
-     * TODO: This should be done by a Manager (bspw. MenuManager
-     */
-    private void setInputProcessors(){
-        InputMultiplexer multi = new InputMultiplexer();
-        
-        // add the keyboard Input Processor
-        multi.addProcessor(inputKeyboardSystem);
-        
-        Gdx.input.setInputProcessor(multi);
-    }
+		// create and add systems
+		addSystems();
+		// create and add input processors
+		setInputProcessors();
 
-    public PooledEngine getEngine() {
-        return engine;
-    }
+		// zu testzwecken
+		ortho = new OrthographicCamera();
+		ortho.setToOrtho(
+				true,
+				Gdx.graphics.getWidth()
+						/ EntityCreator.physicsSystem.getScale(),
+				Gdx.graphics.getHeight()
+						/ EntityCreator.physicsSystem.getScale());
+		debugRenderer = new Box2DDebugRenderer();
 
-    @Override
-    public void dispose() {
-        engine.getSystem(PhysicsSystem.class).getWorld().dispose();
+		MenuEntityCreator.createMainMenu();
+	}
 
-    }
+	private void addSystems() {
 
-    @Override
-    public void pause() {
+		// InputKeyboardSystem
+		inputKeyboardSystem = new InputKeyboardSystem(
+				GameConstants.INPUT_PRIORITY);
+		engine.addSystem(inputKeyboardSystem);
 
-    }
+		// add PhysicSystem
+		PhysicsSystem physicsSystem = new PhysicsSystem(
+				GameConstants.BOX2D_VELOCITY_ITERATIONS,
+				GameConstants.BOX2D_POSITIONS_ITERATIONS,
+				GameConstants.BOX2D_SCALE, GameConstants.PHYSICS_PRIORITY);
 
-    
-    @Override
-    public void render() {
-        engine.update(Gdx.graphics.getDeltaTime());
-        // zu testzwecken
-        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
-        debugRenderer.render(EntityCreator.physicsSystem.getWorld(), ortho.combined);
-    }
+		engine.addSystem(physicsSystem);
+		physicsSystem.setGravity(new Vector2(0, 0)); // erstmal keine gravity,
+														// brauchen wir in
+														// unserem Spiel nicht
+		EntityCreator.physicsSystem = physicsSystem;
 
-    @Override
-    public void resize(int arg0, int arg1) {
-        // TODO Auto-generated method stub
+		// add MovementSystem
+		engine.addSystem(new MovementSystem(GameConstants.PHYSICS_PRIORITY + 1));
 
-    }
+		// add UpdatePositionSystem
+		engine.addSystem(new UpdatePositionSystem(
+				GameConstants.PHYSICS_PRIORITY + 2));
+	}
 
-    @Override
-    public void resume() {
-        // TODO Auto-generated method stub
+	/**
+	 * TODO: This should be done by a Manager (bspw. MenuManager
+	 */
+	private void setInputProcessors() {
+		InputMultiplexer multi = new InputMultiplexer();
 
-    }
+		// add the keyboard Input Processor
+		multi.addProcessor(inputKeyboardSystem);
+
+		Gdx.input.setInputProcessor(multi);
+	}
+
+	public PooledEngine getEngine() {
+		return engine;
+	}
+
+	@Override
+	public void dispose() {
+		engine.getSystem(PhysicsSystem.class).getWorld().dispose();
+
+	}
+
+	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void render() {
+		engine.update(Gdx.graphics.getDeltaTime());
+		// zu testzwecken
+		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+		debugRenderer.render(EntityCreator.physicsSystem.getWorld(),
+				ortho.combined);
+	}
+
+	@Override
+	public void resize(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+
+	}
 
 }
