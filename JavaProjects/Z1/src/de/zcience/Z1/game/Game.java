553f44c34f67aeed4f7d0dea6f1d7697d9144685
Zcience <david.liebemann@gmail.com>
2015-09-29 23:42:42 +0200

    added menu menu is inputprocessor and screen. methods need to be filled. nothing is drawn yet.

--- JavaProjects/Z1/src/de/zcience/Z1/game/Game.java
+++ JavaProjects/Z1/src/de/zcience/Z1/game/Game.java
@@ -1,18 +1,44 @@
 package de.zcience.Z1.game;
 
-import java.util.ArrayList;
+import java.util.Map;
 
 import com.badlogic.ashley.core.PooledEngine;
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
 
 public class Game implements ApplicationListener{
 
-	private PooledEngine engine;
-	private ArrayList<Screen> screens;
+	// Ashley engine
+	public PooledEngine engine;
+	
+	// Screens and their names
+	public Map<String, Screen> screens;
+	
+	// manages the InputProcessors
+	private InputMultiplexer multiplexer;
 	
 	@Override
 	public void create() {
+		// create ashley engine
 		engine = new PooledEngine();
+		
+		// create multiplexer
+		multiplexer = new InputMultiplexer();
+		Gdx.input.setInputProcessor(multiplexer);
+		
+		//create main menu
+		Menu menu = new Menu();
+		screens.put("Menu", menu);
+		multiplexer.addProcessor(menu);
+		fillMenu();
+		
+	}
+
+	private void fillMenu() {
+		// TODO actually add methods to the menu class to add buttons. 
+		// determine, how they should be drawn.
+		
 		
 	}
 
--- JavaProjects/Z1/src/de/zcience/Z1/game/Screen.java
+++ JavaProjects/Z1/src/de/zcience/Z1/game/Screen.java
@@ -1,13 +1,10 @@
 package de.zcience.Z1.game;
 
-public interface Screen {
-	public void create();
-	
+public interface Screen {	
 	
 	public void resize(int width, int height);
 	public void update(float deltaTime);
 	public void pause();
 	public void resume();
 	public void dispose();
-	
 }
